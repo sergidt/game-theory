b9183d10e4db304b2cd4206acb52b5ca95f26462
Sergi Dote <sergi.dote@bizerba.com>
2024-08-14 09:13:23 +0200

    texture

--- src/app/board.component.ts
+++ src/app/board.component.ts
@@ -2,9 +2,10 @@ import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@ang
 import { extend, injectLoader, NgtArgs, NgtSelection } from 'angular-three';
 import { NgtsCameraControls, NgtsOrbitControls } from 'angular-three-soba/controls';
 import { NgtsEnvironment } from 'angular-three-soba/staging';
-import {  Color, Group, Material, Mesh, MeshBasicMaterial, MeshStandardMaterial, ShapeGeometry } from 'three';
+import {  Color, Group, Material, Mesh, MeshBasicMaterial, MeshStandardMaterial, ShapeGeometry, SRGBColorSpace, TextureLoader } from 'three';
 import { GLTFLoader } from 'three-stdlib';
 import { Colour } from './definitions';
+import * as THREE from 'three';
 
 extend({ Mesh, ShapeGeometry, MeshBasicMaterial });
 
@@ -48,86 +49,95 @@ extend({ Mesh, ShapeGeometry, MeshBasicMaterial });
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Board {
+texture = injectLoader(() => TextureLoader, () => '/assets/wood.jpg',
+{
+  onLoad: console.log
+});
+
     board = injectLoader(() => GLTFLoader, () => '/assets/board.glb');
 
 
     roundBigDark1 = injectLoader(() => GLTFLoader, () => '/assets/round-big-1.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene, '42407D')
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene, '42407D')
     });
 
     roundBigDark2 = injectLoader(() => GLTFLoader, () => '/assets/round-big-2.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene, '42407D')
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene, '42407D')
     });
 
     roundSmallDark1 = injectLoader(() => GLTFLoader, () => '/assets/round-small-1.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene, '42407D')
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene, '42407D')
     });
 
     roundSmallDark2 = injectLoader(() => GLTFLoader, () => '/assets/round-small-2.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene, '42407D')
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene, '42407D')
     });
 
     squareBigDark1 = injectLoader(() => GLTFLoader, () => '/assets/square-big-1.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene, '42407D')
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene, '42407D')
     });
 
     squareBigDark2 = injectLoader(() => GLTFLoader, () => '/assets/square-big-2.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene, '42407D')
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene, '42407D')
     });
 
     squareSmallDark1 = injectLoader(() => GLTFLoader, () => '/assets/square-small-1.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene, '42407D')
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene, '42407D')
     });
 
     squareSmallDark2 = injectLoader(() => GLTFLoader, () => '/assets/square-small-2.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene, '42407D')
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene, '42407D')
     });
 
     // Light
 
     roundBigLight1 = injectLoader(() => GLTFLoader, () => '/assets/round-big-1.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene)
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene)
     });
 
     roundBigLight2 = injectLoader(() => GLTFLoader, () => '/assets/round-big-2.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene)
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene)
     });
 
     roundSmallLight1 = injectLoader(() => GLTFLoader, () => '/assets/round-small-1.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene)
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene)
     });
 
     roundSmallLight2 = injectLoader(() => GLTFLoader, () => '/assets/round-small-2.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene)
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene)
     });
 
     squareBigLight1 = injectLoader(() => GLTFLoader, () => '/assets/square-big-1.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene)
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene)
     });
 
     squareBigLight2 = injectLoader(() => GLTFLoader, () => '/assets/square-big-2.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene)
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene)
     });
 
     squareSmallLight1 = injectLoader(() => GLTFLoader, () => '/assets/square-small-1.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene)
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene)
     });
 
     squareSmallLight2 = injectLoader(() => GLTFLoader, () => '/assets/square-small-2.glb', {
-      onLoad:  ({scene}: {scene: Group}) => setMaterialColor(scene)
+      onLoad:  ({scene}: {scene: Group}) => this.setMaterialColor(scene)
     });
 
     log(event: Event) {
         console.log(event);
     }
+
+    setMaterialColor(scene: Group, color: string = 'FEEEAD')  {
+      console.log(scene);
+//console.log(this.texture());
+      const mesh = scene.getObjectByName('imagetostl_mesh0') as Mesh;
+      const materials: Material|Material[] = mesh.material;
+      const texture = this.texture();
+      //texture!.colorSpace = THREE.SRGBColorSpace;
+      const material = new MeshBasicMaterial({map: texture});
+      mesh.material = material;
+     // material.color.setHex(parseInt(color, 16));
+    }
 }
 
-function setMaterialColor(scene: Group, color: string = 'FEEEAD')  {
-  console.log(scene);
-  /*const mesh = scene.getObjectByName('imagetostl_mesh0') as Mesh;
-  const materials: Material|Material[] = mesh.material;
-  const material = new MeshBasicMaterial();
-  mesh.material = material;
-  material.color.setHex(parseInt(color, 16));*/
-}
 
